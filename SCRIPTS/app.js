e9e054a48f42087c6430c778aa17473f38155c26
Adam Duda <97346707+AdamDuda1@users.noreply.github.com>
2022-01-28 11:27:46 +0100

    homepage

---
+++ SCRIPTS/app.js
@@ -0,0 +1,33 @@
+
+    _method = GET
+    linkHREF = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+    _userAPIID = localStorage.getItem("_userAPIID")
+    _userID = localStorage.getItem("_userID")
+    sessionStorage.setItem("_method", _method);
+    function go() {
+        if (_userAPIID = false) {
+            localStorage.setItem("_userAPIID", true);
+            localStorage.setItem("_userID", Math.random());
+            localStorage.setItem("link", linkHREF);
+            localStorage.setItem("_method", _method);
+            localStorage.setItem("$token", Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1) + Math.floor(Math.random()*9+1) 
+            + Math.floor(Math.random()*9+1));
+        }
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+    }
+    // © ️ by Adam Duda, All rights reserved.

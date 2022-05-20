8b5eb2d2ffa8484033baa79c07a89780ff4c6f57
Adam Duda <adam.duda@post.com>
2022-05-20 20:00:05 +0200

    Up #21

---
+++ scripts_useragent.js
@@ -0,0 +1,16 @@
+tempuserid;
+maineventhandler;
+
+const maineventhandler = document.querySelector('body');
+
+function GetUserData(protocol = GET, tempuserid) {
+
+}
+
+function SetTempUserId(tempuserid) {
+
+}
+
+maineventhandler.AddEventListener('load', function ASyncRequest(tempuserid) {
+
+});
